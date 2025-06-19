4a8fec8fcca1b9943ea342ab13fc0a268af26a55
75BOSS <mccristiancaqt@gmail.com>
2025-06-19 09:41:04 -0500

    Cambios get grafi9

--- includes/header_tecnico.php
+++ includes/header_tecnico.php
@@ -1,38 +1,36 @@
 <?php
-session_start();
-if (!isset($_SESSION['nombre']) || $_SESSION['rol'] !== 'tecnico') {
-    header('Location: ../login.php');
-    exit;
-}
+if (!isset($_SESSION)) session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Panel del Técnico</title>
+    <title>Panel Técnico</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="dashboard_tecnico.php">Monitoreo Técnico</a>
+        <a class="navbar-brand" href="#">Técnico</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
+        <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="dashboard_tecnico.php">Sensores</a>
+                    <a class="nav-link" href="dashboard.php">Sensores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reparar_sensor.php">Reparar Sensor</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="alertas.php">Alertas</a>
                 </li>
             </ul>
-            <span class="navbar-text me-3">
-                Técnico: <?= htmlspecialchars($_SESSION['nombre']) ?>
+            <span class="navbar-text text-white">
+                Técnico: <?= $_SESSION['nombre'] ?>
+                <a href="../logout.php" class="btn btn-sm btn-light ms-3">Cerrar sesión</a>
             </span>
-            <a href="../logout.php" class="btn btn-outline-light btn-sm">Cerrar sesión</a>
         </div>
     </div>
 </nav>
