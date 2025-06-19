f41cacfaae2ece43d11b087c2e8db50c78fa44f4
75BOSS <mccristiancaqt@gmail.com>
2025-06-19 06:23:54 -0500

    cambios generados nuevos base

--- conexion.php
+++ conexion.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$host = "localhost";
+$host = "localhost"; // Cambia si en el panel dice otro host, como una IP
 $user = "u240362798_sistemaagua";
-$pass = "glcp.,?2A.";
+$pass = "Lap2incasablanca1"; // Nueva contraseña
 $db   = "u240362798_sistemaagua";
 
 $conexion = new mysqli($host, $user, $pass, $db);
